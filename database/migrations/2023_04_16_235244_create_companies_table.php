7667f451d9f7161cb628f689baa40994ad7829a1
0xyzone <58982560+0xyzone@users.noreply.github.com>
2023-04-17 05:43:35 +0545

    added company migration

--- database/migrations/2023_04_16_235244_create_companies_table.php
+++ database/migrations/2023_04_16_235244_create_companies_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('company', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->string('name')->nullable();
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('company');
+        Schema::dropIfExists('companies');
     }
 };
